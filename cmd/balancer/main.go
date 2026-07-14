fda7719dd337549078b0cca5a613343495c47539
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-07-14 12:01:52 +0300

    Big fix of main, correct starting of server etc

--- cmd/balancer/main.go
+++ cmd/balancer/main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 	"time"
 	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"go-load-balancer/internal/backend"
 	"go-load-balancer/internal/balancer"
@@ -14,49 +18,74 @@ import (
 
 func main() {
 	configPath := "configs/config.yaml"
-	config, err := config.Load(configPath)
+	cfg, err := config.Load(configPath)
 	if err != nil {
-		log.Printf("Import config error: %v", err)
-		return
+		log.Fatalf("Import config error: %v", err)
 	}
 
+	confBacks := cfg.Backends
 	backends := []*backend.Backend{}
-	confBacks := config.Backends
 	for i := 0; i < len(confBacks); i++ {
 		back, err := backend.NewBackend(confBacks[i].URL)
 		if err != nil {
-			log.Printf("Backend create error: %v", err)
-			return
+			log.Fatalf("Backend create error: %v", err)
 		}
-		backends[i] = back
+		backends = append(backends, back)
 	}
 
-	client := http.Client{
+	client := &http.Client{
 		Timeout: 5 * time.Second,
 	}
 
-	var myBalancer balancer.Balancer
-	if config.Strategy == "round_robin" {
-		myBalancer = *balancer.NewBalancer(
+	var myBalancer *balancer.Balancer
+	if cfg.Strategy == "round-robin" {
+		myBalancer = balancer.NewBalancer(
 			backends,
 			&strategy.RoundRobin{},
-			&client,
+			client,
 		)
-	} else if config.Strategy == "least_conn" {
-		myBalancer = *balancer.NewBalancer(
+	} else if cfg.Strategy == "least-conn" {
+		myBalancer = balancer.NewBalancer(
 			backends,
 			&strategy.LeastConn{},
-			&client,
+			client,
 		)
+	} else {
+		log.Fatal("Unknown strategy")
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	go func() {
-		myBalancer.RunHealthCheck(ctx, time.Second * 1)
-		cancel()
+		myBalancer.RunHealthCheck(ctx, cfg.HealthCheckInterval)
 	}()
 
-	var w http.ResponseWriter
-	var r *http.Request
-	myBalancer.ServeHTTP(w, r)
+	server := &http.Server{
+    Addr: fmt.Sprintf(":%d", cfg.Port),
+    Handler: myBalancer,
+	}
+
+	go func() {
+		log.Printf("Balancer started on :%d", cfg.Port)
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	cancel()
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	errSD := server.Shutdown(shutdownCtx)
+	if errSD != nil {
+		log.Fatalf("Stop server error: %v", errSD)
+	}
+
+	log.Println("Server stopped")
 }
